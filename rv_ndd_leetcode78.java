f86b766faa7bcb873dffe41d6578fa2ef496acb0
MLA98 <yuldong@ucdavis.edu>
2019-05-12 01:06:47 -0700

    Create rv_ndd_leetcode78.java

---
+++ rv_ndd_leetcode78.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        bthelper(ans, temp, nums, 0);
+        return ans;
+    }
+    
+    private void bthelper(List<List<Integer>> ans, List<Integer> temp, int[] nums, int start){
+        ans.add(new ArrayList<>(temp));
+        for (int i = start; i < nums.length; i ++){
+            temp.add(nums[i]);
+            bthelper(ans, temp, nums, i);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
+
+// feels bad, it should be easy but I could not finish it by myself.!!
+// Look up ans, then do it.
+// Need review and practice!!
