e89149992caec541ab19b3723660030beb493791
MLA98 <yuldong@ucdavis.edu>
2019-06-26 16:46:27 +0800

    Update lc160.java

--- lc160.java
+++ lc160.java
@@ -27,3 +27,36 @@ public class Solution {
         return null;
     }
 }
+
+
+// Faster one
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    // go through a. string first and use hashset to store all of them.
+    // go through b. string then, check which one is duplicated.
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null){
+            return null;
+        }
+        ListNode p = headA;
+        ListNode q = headB;
+        while(p != null || q != null){
+            if (p == q){
+                return p;
+            }
+            p = p == null ? headB: p.next;
+            q = q == null ? headA: q.next;
+        }
+        return null;
+    }
+}
