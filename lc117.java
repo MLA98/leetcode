2b63fbc2f489f3134aaf41a1731fbab749c22cf5
MLA98 <leoyoudatou@outlook.com>
2019-12-27 15:34:40 -0800

    refreshed,

--- lc117.java
+++ lc117.java
@@ -1,51 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
 class Solution {
     public Node connect(Node root) {
-        Node rootSaved = root;
-        // GREAT METHOD
+        Node savedRoot = root;
         while(root != null){
-            Node dummy = new Node(0, null, null, null);
-            Node trav = dummy;
+            Node trav = new Node(0, null, null, null);
+            Node storedTrav = trav;
             while(root != null){
                 if(root.left != null){
                     trav.next = root.left;
                     trav = trav.next;
                 }
+
                 if(root.right != null){
                     trav.next = root.right;
                     trav = trav.next;
                 }
                 root = root.next;
             }
-            root = dummy.next;
-        }
-        return rootSaved;
-    }
-}
-
-// O(n), O(n) rule violated
-class Solution {
-    public Node connect(Node root) {
-        if(root == null){
-            return null;
+            
+            root = storedTrav.next;
         }
         
-        Queue<Node> q = new LinkedList<>();
-        q.offer(root);
-        
-        while(!q.isEmpty()){
-            int layer = q.size();
-            for(int i = 0; i < layer; i ++){
-                Node node = q.poll();
-                node.next = i + 1 == layer? null: q.peek();
-                if(node.left != null){
-                    q.offer(node.left);
-                }
-                if(node.right != null){
-                    q.offer(node.right);
-                }
-            }
-        }
-        
-        return root;
+        return savedRoot;
     }
 }
